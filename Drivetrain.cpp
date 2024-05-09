cb66dc7e099978b74b738135b125875aa0933b5c
Nicholas Beach <nicholascbeach@gmail.com>
2024-05-08 21:42:57 -0400

    Abstracted drivetrain to class

--- Drivetrain.cpp
+++ Drivetrain.cpp
@@ -1,19 +1,24 @@
-// #include "CytronMotorDriver.h"
+#include "CytronMotorDriver.h"
 
-// class Drivetrain {
-//   private:
-//   CytronMD rearLeftMotor;
-//   CytronMD rearRightMotor;
+class Drivetrain {
+  CytronMD* frontLeftMotor;
+  CytronMD* frontRightMotor;
+  CytronMD* rearLeftMotor;
+  CytronMD* rearRightMotor;
 
-//   public: 
-//   Drivetrain() {
-//     CytronMD rearLeftMotor = CytronMD(PWM_DIR, 3, 2);
-//     CytronMD rearRightMotor = CytronMD(PWM_DIR, 6, 7);
-//   }
+  public: 
+  Drivetrain() {
+    CytronMD frontLeftMotor = CytronMD(PWM_DIR, 5, 4);
+    CytronMD frontRightMotor = CytronMD(PWM_DIR, 10, 12);
+    CytronMD rearLeftMotor = CytronMD(PWM_DIR, 3, 2);
+    CytronMD rearRightMotor = CytronMD(PWM_DIR, 6, 7);
+  }
 
-//   void setSpeed(int speed) {
-//     rearLeftMotor.setSpeed(speed); 
-//     rearRightMotor.setSpeed(speed);
-//   }
+  void setSpeed(int speed) {
+    frontLeftMotor->setSpeed(speed);
+    frontRightMotor->setSpeed(speed);
+    rearLeftMotor->setSpeed(speed); 
+    rearRightMotor->setSpeed(speed);
+  }
 
-// };
+};
--- kid-car-controller.ino
+++ kid-car-controller.ino
@@ -1,31 +1,26 @@
 #include <SoftwareSerial.h>
-#include "CytronMotorDriver.h"
-#include "Cytron_PS2Shield.h"
+// #include "CytronMotorDriver.h"
 #include "Controller.cpp"
+#include "Drivetrain.cpp"
 
-// Cytron_PS2Shield ps2(8,9);
 Controller controller;
-CytronMD frontLeftMotor(PWM_DIR, 5, 4);
-CytronMD frontRightMotor(PWM_DIR, 10, 12);
-CytronMD rearLeftMotor(PWM_DIR, 3, 2);
-CytronMD rearRightMotor(PWM_DIR, 6, 7);
+Drivetrain drivetrain;
+
 
 void setup()
 {
-  controller = Controller();
-  // ps2.begin(9600);
   Serial.begin(9600);
+  controller = Controller();
+  drivetrain = Drivetrain();
   delay(1000);
 }
 
 void loop()
 {
-  int throttle = controller.throttle();
-  Serial.println(throttle);
-  frontLeftMotor.setSpeed(throttle);
-  frontRightMotor.setSpeed(throttle);
-  rearLeftMotor.setSpeed(throttle);
-  rearRightMotor.setSpeed(throttle);
+  int throttlePosition = controller.throttlePosition();
+  Serial.println(throttlePosition);
+  drivetrain.setSpeed(throttlePosition);
+
   delay(100);
 }
 
