7824d23c34e83536d2b991e75b47c9975dbf28ab
Nicholas Beach <nicholascbeach@gmail.com>
2024-08-09 20:26:12 -0400

    Revert "Simplified PWM frequency for now"
    This reverts commit 4ee44fdb9b7a477e0ed831bb56f3247162534e13.

--- src/motor/Motor.cpp
+++ src/motor/Motor.cpp
@@ -1,15 +1,21 @@
 #include "Motor.h"
 #include "AbstractMotor.h"
 
-Motor::Motor(uint8_t pwmPin, uint8_t directionPin) {
+Motor::Motor(uint8_t pwmPin, uint8_t directionPin, int pwmFrequency) {
     this->pwmPin = pwmPin;
     this->directionPin = directionPin;
+    this->pwmFrequency = pwmFrequency;
 
     pinMode(pwmPin, OUTPUT);
     pinMode(directionPin, OUTPUT);
     digitalWrite(pwmPin, LOW);
     digitalWrite(directionPin, LOW);
 
+    pwmControl = pwmFrequency == NULL ? NULL : new AVR_PWM(pwmPin, pwmFrequency, 0);
+}
+
+Motor::~Motor() {
+     if(pwmControl != NULL) delete pwmControl;
 }
 
 void Motor::setSpeed(int speed) {
@@ -19,7 +25,12 @@ void Motor::setSpeed(int speed) {
         speed = -255;
     }
 
-    analogWrite(pwmPin, speed >= 0 ? speed : -speed);
+     if(pwmControl == NULL) {
+        analogWrite(pwmPin, speed >= 0 ? speed : -speed);
+     } else {
+        pwmControl->setPWM(pwmPin, pwmFrequency, (speed >= 0 ? speed : -speed) / 255.0f * 100);
+     }
+
     digitalWrite(directionPin, speed >= 0 ? LOW : HIGH);
 }
 
--- src/motor/Motor.h
+++ src/motor/Motor.h
@@ -4,14 +4,18 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include "AbstractMotor.h"
+#include "../vendor/AVR_PWM.h"
 
 class Motor : public AbstractMotor {
     private:
         uint8_t pwmPin;
         uint8_t directionPin;
+        int pwmFrequency;
+        AVR_PWM* pwmControl;
 
     public:
-        Motor(uint8_t pwmPin, uint8_t directionPin);
+        Motor(uint8_t pwmPin, uint8_t directionPin, int pwmFrequency);
+        ~Motor();
         void setSpeed(int speed);
 };
 
